5c62dbdcb5dc57079b5fcadca0d8ec321b5535d9
Stefan <stefanseifeddine@gmail.com>
2024-05-18 20:49:50 +0100

    Add bodyParser and import all the controllers

--- routes/post.js
+++ routes/post.js
@@ -1,33 +1,28 @@
 const express = require("express");
 const router = express.Router();
+bodyParser = require("body-parser").json();
+
+const getAllPosts = require("../controllers/getAllPosts");
+const getPostById = require("../controllers/getPostById");
+const createPost = require("../controllers/createPost");
+const updatePost = require("../controllers/updatePost");
+const deletePost = require("../controllers/deletePost");
 
 // Routes
 // Description: Retrieves all published posts.
-router.get("/posts", (req, res) => {
-    return res.send("Received a GET HTTP method /posts ");
-});
+router.get("/posts", getAllPosts);
 
 // Description: Retrieves a specific post by its ID.
-router.get("/posts/:postId", (req, res) => {
-    return res.send(`Received a GET HTTP method /posts/${req.params.postId}`);
-});
+router.get("/posts/:postId", getPostById);
 
 // Description: Creates a new post.
-router.post("/posts", (req, res) => {
-    return res.send("Received a POST HTTP method /posts ");
-});
+router.post("/posts", bodyParser, createPost);
 
 // Description: Updates an existing post by its ID.
-router.put("/posts/:postId", (req, res) => {
-    return res.send(`Received a PUT HTTP method /posts/${req.params.postId} `);
-});
+router.put("/posts/:postId", bodyParser, updatePost);
 
 // Description: Deletes a post by its ID.
-router.delete("/posts/:postId", (req, res) => {
-    return res.send(
-        `Received a DELETE HTTP method /posts/${req.params.postId} `
-    );
-});
+router.delete("/posts/:postId", deletePost);
 
 // Export router
 module.exports = router;
