12a2859f0b9836e1d31e5da10b5a95ffcabecee1
Stefan <stefanseifeddine@gmail.com>
2024-05-21 19:43:22 +0100

    Add controllers

--- routes/publishing.js
+++ routes/publishing.js
@@ -1,25 +1,20 @@
 const express = require("express");
 const router = express.Router();
+bodyParser = require("body-parser").json();
+
+const getUnpublishedPosts = require("../controllers/getUnpublishedPosts");
+const publishPost = require("../controllers/publishPost");
+const unpublishPost = require("../controllers/unpublishPost");
 
 // Routes
 // Description: Retrieves all unpublished posts.
-router.get("/posts/unpublished", (req, res) => {
-    return res.send(`Received a GET HTTP method /posts/unpublished`);
-});
+router.get("/posts/unpublished", getUnpublishedPosts);
 
 // Description: Publishes a post.
-router.put("/posts/:postId/publish", (req, res) => {
-    return res.send(
-        `Received a PUT HTTP method /posts/${req.params.postId}/publish`
-    );
-});
+router.put("/posts/:postId/publish", bodyParser, publishPost);
 
 // Description: Unpublishes a post.
-router.put("/posts/:postId/unpublish", (req, res) => {
-    return res.send(
-        `Received a PUT HTTP method /posts/${req.params.postId}/unpublish`
-    );
-});
+router.put("/posts/:postId/unpublish", bodyParser, unpublishPost);
 
 // Export router
 module.exports = router;
