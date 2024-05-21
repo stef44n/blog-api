15941a4e33df077152e9e5f1b1d9b080e2aaa96d
Stefan <stefanseifeddine@gmail.com>
2024-05-21 15:22:01 +0100

    Import controllers for CRUD operations

--- routes/comment.js
+++ routes/comment.js
@@ -1,33 +1,23 @@
 const express = require("express");
 const router = express.Router();
+bodyParser = require("body-parser").json();
+
+const createComment = require("../controllers/createComment");
+const getCommentsForPost = require("../controllers/getCommentsForPost");
+const updateComment = require("../controllers/updateComment");
+const deleteComment = require("../controllers/deleteComment");
 
 // Description: Retrieves all comments for a specific post.
-router.get("/posts/:postId/comments", (req, res) => {
-    return res.send(
-        `Received a GET HTTP method /posts/${req.params.postId}/comments `
-    );
-});
+router.get("/posts/:postId/comments", bodyParser, getCommentsForPost);
 
 // Description: Creates a new comment for a post.
-router.post("/posts/:postId/comments", (req, res) => {
-    return res.send(
-        `Received a POST HTTP method /posts/${req.params.postId}/comments `
-    );
-});
+router.post("/posts/:postId/comments", bodyParser, createComment);
 
 // Description: Updates an existing comment.
-router.put("/posts/:postId/comments/:commentId", (req, res) => {
-    return res.send(
-        `Received a PUT HTTP method /posts/${req.params.postId}/comments/${req.params.commentId} `
-    );
-});
+router.put("/posts/:postId/comments/:commentId", bodyParser, updateComment);
 
 // Description: Deletes a comment.
-router.delete("/posts/:postId/comments/:commentId", (req, res) => {
-    return res.send(
-        `Received a DELETE HTTP method /posts/${req.params.postId}/comments/${req.params.commentId} `
-    );
-});
+router.delete("/posts/:postId/comments/:commentId", deleteComment);
 
 // Export router
 module.exports = router;
