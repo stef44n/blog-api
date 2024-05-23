93b0b7cdaf65c4d53e77dc00bd42632e5ee306c0
Stefan <stefanseifeddine@gmail.com>
2024-05-23 21:14:58 +0100

    Uncomment code to allow testing

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -5,22 +5,22 @@ const User = require("../models/user");
 // router.post("/sign-up", async (req, res, next) => {
 // TURNS INTO
 const signUpController = async (req, res, next) => {
-    return res.send("Received a POST HTTP method /sign-up ");
+    // return res.send("Received a POST HTTP method /sign-up ");
 
-    // try {
-    //     bcryptjs.hash(req.body.password, 10, async (err, hashedPassword) => {
-    //         // if err, do something
-    //         // otherwise, store hashedPassword in DB
-    //         const user = new User({
-    //             username: req.body.username,
-    //             password: hashedPassword,
-    //         });
-    //         const result = await user.save();
-    //     });
-    //     res.redirect("/");
-    // } catch (err) {
-    //     return next(err);
-    // }
+    try {
+        bcryptjs.hash(req.body.password, 10, async (err, hashedPassword) => {
+            // if err, do something
+            // otherwise, store hashedPassword in DB
+            const user = new User({
+                username: req.body.username,
+                password: hashedPassword,
+            });
+            await user.save();
+        });
+        res.redirect("/");
+    } catch (err) {
+        return next(err);
+    }
 };
 
 module.exports = signUpController;
